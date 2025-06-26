ec53bab508f081071e3b6c87f7f877bd755e72a1
PuchuriEliasUCH <elipuchuries@uch.pe>
2025-06-26 04:23:38 -0500

    api, puedes funcionar... pofavo?

--- config/database.php
+++ config/database.php
@@ -1,12 +1,22 @@
 <?php
-class Database {    
-    private $host =     getenv('MYSQL_ADDON_HOST');
-    private $database = getenv('MYSQL_ADDON_DB');
-    private $username = getenv('MYSQL_ADDON_USER');
-    private $password = getenv('MYSQL_ADDON_PASSWORD');
+class Database
+{
+    private $host;
+    private $database;
+    private $username;
+    private $password;
     private $conn;
 
-    public function connect() {
+    public function __construct()
+    {
+        $this->host = getenv('MYSQL_ADDON_HOST');
+        $this->database = getenv('MYSQL_ADDON_DB');
+        $this->username = getenv('MYSQL_ADDON_USER');
+        $this->password = getenv('MYSQL_ADDON_PASSWORD');
+    }
+
+    public function connect()
+    {
         $this->conn = null;
         try {
             $this->conn = new PDO(
@@ -15,10 +25,9 @@ class Database {
                 $this->password
             );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo "Error de conexión: " . $e->getMessage();
         }
         return $this->conn;
     }
 }
-?>
